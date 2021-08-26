3d03d8f40e4b9cc7c77c973a124d19bbfcc0f5c2
user <user@LAPTOP-B2VSR0FD>
2021-08-26 15:19:42 +0900

    n부터 m까지의 합

--- MVCProject/src/main/java/kr/hs/study/controller/HomeController.java
+++ MVCProject/src/main/java/kr/hs/study/controller/HomeController.java
@@ -1,9 +1,10 @@
 package kr.hs.study.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,6 +15,25 @@ public class HomeController {
 		return "index";
 	}
 	
+    @GetMapping("/sum")
+	public String sum() {
+		return "sum";
+	}
+	@PostMapping("/sum")
+	public String sum2(@RequestParam int n, @RequestParam int m, Model model) {
+		int sum = 0;
+		
+		if(n>m) {
+			int temp = n;
+			n = m;
+			m = temp;
+		}
+		for(int i=n; i<=m; i++) {
+			sum += i;
+		}
+		model.addAttribute("sum", n+"부터 "+m+"까지의 합은 "+sum);
+		return "sum";
+	}
 	
 	
 
